9edb9d441c9687744668b9d04fa18331dd4feb28
Christian Weber <ChristianTMWeber@gmail.com>
2016-04-24 14:30:19 -0400

    Implemented templates. ThreeVector is now a templated class. All ThreeVector methods that have been implemented so far, work now with the template. As a result all the implementations moved from ThreeVector.cxx to ThreeVector.h The 'ThreeVectorTest.dev' has been adjusted accurdingly. I.e. 'ThreeVector.cxx' is not listed in it anymore and this won't be taking into account when compiling
    In a future version I will delete ThreeVector.cxx

--- ThreeVector.h
+++ ThreeVector.h
@@ -13,43 +13,46 @@ ThreeVector - A class that implements (real valued) three component vectors in C
 #ifndef ThreeVector_h
 #define ThreeVector_h
 #include <iostream> // for cin, cout
-
-// class delceration
+#include <cmath> 
+// class decleration
+template <typename aType>
 class ThreeVector{
 private:
 	// these are the three private variables that hold the three elements of the vector
-	double mx, my, mz;
+	aType mx, my, mz;
 
 public:
 	////constructors
-	//default constructor
-	ThreeVector();
-	// set constructor
-	ThreeVector(double x, double y, double z);
-	//copy constructor
-	ThreeVector(const ThreeVector &copyMeVector);
+	ThreeVector();	//default constructor
+	ThreeVector(aType x, aType y, aType z); 	// set constructor
+	ThreeVector(const ThreeVector &copyMeVector); //copy constructor
 
 	//Destructor
-	~ThreeVector();
+	//~ThreeVector();
 
 	//// access members
 	//return each element of the vector individually
 	//since none of these methods are changing anything, we make it explicit by declaring them as const. This will also allow us to use them on a ThreeVector instance that has been declared as constant 
-	double x()const; //return x component of 3 vector
-	double y()const; //return y component of 3 vector
-	double z()const; //return z component of 3 vector
+	
+	aType x()const; //return x component of 3 vector
+	aType y()const; //return y component of 3 vector
+	aType z()const; //return z component of 3 vector
 
-	double mag()const; //return the magnitude of the vector
-	double phi()const; //return the azimuthal angle of the vector in the xy plane
-	double theta()const; //return the polar angle of the vector
-	double perp()const; //return the transverse (to z) component of the vector
-	friend std::ostream& operator<<(std::ostream& os, const ThreeVector& vOut);
+	aType mag()const; //return the magnitude of the vector
+	aType phi()const; //return the azimuthal angle of the vector in the xy plane
+	aType theta()const; //return the polar angle of the vector
+	aType perp()const; //return the transverse (to z) component of the vector
+	
+	//output operator
+	template<typename bType>
+	friend std::ostream& operator<<(std::ostream& os, const ThreeVector<bType>& vOut);
+	//http://stackoverflow.com/questions/4660123/overloading-friend-operator-for-template-class
 
 	//// set members
 	//set each element of the vector individually
-	void setX(double val); //set x component of 3 vector
-	void setY(double val); //set y component of 3 vector
-	void setZ(double val); //set z component of 3 vector
+	void setX(aType val); //set x component of 3 vector
+	void setY(aType val); //set y component of 3 vector
+	void setZ(aType val); //set z component of 3 vector
 
 	//Assignment Operator
 	ThreeVector operator=(const ThreeVector& assignMeVector);
@@ -57,4 +60,66 @@ public:
 
 };
 
+
 #endif
+
+////constructors
+
+template<typename aType>  		//default constructor
+ThreeVector<aType>::ThreeVector(){mx=0; my=0; mz=0;}
+template<typename aType> 		// set constructor
+ThreeVector<aType>::ThreeVector(aType x, aType y, aType z){setX(x); setY(y); setZ(z);} 
+template<typename aType> 		//copy constructor
+ThreeVector<aType>::ThreeVector(const ThreeVector &copyMeVector){ setX(copyMeVector.x()); setY(copyMeVector.y()); setZ(copyMeVector.z());}
+//unless the methods .x, .y, .z are also declared as constant, we will get an error here.
+
+////Destructor
+//ThreeVector::~ThreeVector(){}
+
+
+//// access methods
+//return each element of the vector individually
+
+template <typename aType>
+aType ThreeVector<aType>::x()const{return mx;} //return x component of 3 vector
+template <typename aType>
+aType ThreeVector<aType>::y()const{return my;} //return y component of 3 vector
+template <typename aType>
+aType ThreeVector<aType>::z()const{return mz;} //return z component of 3 vector
+
+
+template <typename aType>
+aType ThreeVector<aType>::mag()const{return sqrt(pow(x(),2)+pow(y(),2)+pow(z(),2));} //return the magnitude of the vector
+template <typename aType> 
+aType ThreeVector<aType>::phi()const{return atan2(y(),x());} //return the azimuthal angle of the vector in the xy plane
+template <typename aType> 
+aType ThreeVector<aType>::theta()const{ return acos(z()/mag());} //return the polar angle of the vector
+template <typename aType> 
+aType ThreeVector<aType>::perp()const{return sqrt(pow(x(),2)+pow(y(),2));} //return the transverse (to z) component of the vector
+
+template <typename aType> 
+std::ostream& operator<<(std::ostream& os, const ThreeVector<aType>& vOut){
+	os << vOut.x() <<"\t"<< vOut.y() <<"\t"<< vOut.z();
+	return os;}
+
+
+//// set set methods
+//set each element of the vector individually
+template <typename aType>
+void ThreeVector<aType>::setX(aType val){mx = val;} //set y component of 3 vector
+template <typename aType>
+void ThreeVector<aType>::setY(aType val){my = val;} //set y component of 3 vector
+template <typename aType>
+void ThreeVector<aType>::setZ(aType val){mz = val;} //set z component of 3 vector
+
+//Assignment Operator
+template <typename aType>
+ThreeVector<aType> ThreeVector<aType>::operator=(const ThreeVector& assignMeVector){ setX(assignMeVector.x()); setY(assignMeVector.y()); setZ(assignMeVector.z());
+return *this;}//This Assignment Operator is self-assignment safe, e.g. nothing gets deleted in any part of the self asignment process. And being self-assignment safe is preferably, to having an operator that needs protected from self assignment. So we do not even check here for self assignment.
+//NOTE, assigments are right now only between ThreeVector<aType> of the same <aType> supported
+
+////in case the method implementations are in a seperate .cxx file (i.e. seperate from the .h file)
+// instantiate the ThreeVector class for the types that I expect to use -- I don't wanna write everything into the .h file, despite that being the other recommended solution to the otherwise extant linking problem
+// see here for an explanation and example solutions to the problem -- https://isocpp.org/wiki/faq/templates#templates-defn-vs-decl
+//template class ThreeVector<double>;
+//template class ThreeVector<int>;
--- ThreeVectorTest.cxx
+++ ThreeVectorTest.cxx
@@ -21,90 +21,101 @@ using namespace std;
 
 //declarations for functions that we want to use
 
-void allElementsEqual(  const int &n,const ThreeVector &vA, const ThreeVector &vB);
-void allElementsUnequal(const int &n,const ThreeVector &vA, const ThreeVector &vB);
+void allElementsEqual(  const int &n,const ThreeVector<double> &vA, const ThreeVector<double> &vB);
+void allElementsUnequal(const int &n,const ThreeVector<double> &vA, const ThreeVector<double> &vB);
 
 //TEST
 //implement the output operator overloading for the ThreeVector class
 //ostream& operator<<(ostream& os, const ThreeVector& vOut);
 
 int main(int argc, char *argv[]) {
-	const double pi = 3.141592653589793;
-	const double lim = 1E-15;
 	int n=0;
 
 	cout << "Begin ThreeVectorTest" << endl;
 
 	//create an vector using the default constructor
-	ThreeVector v1;
-	//check the default constructor
+	ThreeVector<double> v1;
+	ThreeVector<int> v1_int;
+	//check the default constructor and the methods to get the x,y, and z components
 	n=1;
 	if((v1.x()==0)&&(v1.y()==0)&&(v1.z()==0)) {cout << "Test  "<<n<<" passed!"<< endl;}
 	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
-	//create an vector using the default constructor
-	ThreeVector v2(1,2,3);
-	//check the set constructor
-	n=2;
-	if((v2.x()==1)&&(v2.y()==2)&&(v2.z()==3)) {cout << "Test  "<<n<<" passed!"<< endl;}
-	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
+
 	//check the set method
-	n=3;
+	n=2;
 	v1.setX(4);		v1.setY(5);		v1.setZ(6);
 	if((v1.x()==4)&&(v1.y()==5)&&(v1.z()==6)) {cout << "Test  "<<n<<" passed!"<< endl;}
 	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
+
+	//check the set constructor
+	ThreeVector<double> v2(1,2,3);
+	n=3;
+	if((v2.x()==1)&&(v2.y()==2)&&(v2.z()==3)) {cout << "Test  "<<n<<" passed!"<< endl;}
+	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
+
 	//check the copy constructor
 	n=4;
-	ThreeVector v3(v1);
+	ThreeVector<double> v3(v1);
 	allElementsEqual(n,v1,v3);
 	//check that we actually copied the vector v1, and not only references the elements of v3 to the locations of the elements of v1
 	n=5;
 	v3.setX(7);		v3.setY(8);		v3.setZ(9);
 	allElementsUnequal(n,v1,v3);
+	
 	//Check Assignment Operator
 	n=6;
-	ThreeVector v6;
+	ThreeVector<double> v6;
 	v6=v3;
 	allElementsEqual(n,v3,v6);
+
 	//check that we actually assign the vector v6, and not only references the elements of v3 to the locations of the elements of v6
 	n=7;
 	v6.setX(2*v6.x());		v6.setY(2*v6.y());		v6.setZ(2*v6.z());
 	allElementsUnequal(n,v3,v6);
 	//What happens if we self reference? Nothing should happen. 
 	n=8;
-	ThreeVector v8;
+	ThreeVector<double> v8;
 	v8=v6;		v6=v6;
 	allElementsEqual(n,v6,v6);
+
 	//test the magnitude method
 	n=9;
-	ThreeVector v9a(1,2,2), v9b(4,4,4);
+	ThreeVector<double> v9a(1,2,2), v9b(4,4,4);
 	if((v9a.mag()==3)&&(v9b.mag()==sqrt(3*16))) {cout << "Test  "<<n<<" passed!"<< endl;}
 	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
+
 	//test the phi method
+	const double pi = 3.141592653589793;
 	n=10;
-	ThreeVector v10a(1,0,0), v10b(0,1,0), v10c(1,1,0), v10d(0,-1,0), v10e(-23,-6,0);
+	ThreeVector<double> v10a(1,0,0), v10b(0,1,0), v10c(1,1,0), v10d(0,-1,0), v10e(-23,-6,0);
 	if((v10a.phi()==0)&&(v10b.phi()==pi/2)&&(v10c.phi()==pi/4)&&(v10d.phi()==-pi/2)&&(v10e.phi()==atan2(-6,-23))) {cout << "Test "<<n<<" passed!"<< endl;}
 	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
+
 	//test the theta method
+
+	const double acc = 1E-15; // the numerical accuracy that I demand, if '==' fails while it shouldn'
 	n=11;
-	ThreeVector v11a(1,0,0), v11b(0,0,1), v11c(0,1,1), v11d(0,2,2), v11e(0,-6,79);
+	ThreeVector<double> v11a(1,0,0), v11b(0,0,1), v11c(0,1,1), v11d(0,2,2), v11e(0,-6,79);
 	if( (    v11a.theta()==pi/2)&&
 		(    v11b.theta()==0)&&
-		(abs(v11c.theta()-pi/4) < lim) &&
-		(abs(v11d.theta()-pi/4) < lim) &&
+		(abs(v11c.theta()-pi/4) < acc) &&
+		(abs(v11d.theta()-pi/4) < acc) &&
 		(    v11e.theta()==acos(79/sqrt(pow(-6,2)+pow(79,2))))
 		){cout << "Test "<<n<<" passed!"<< endl;}
 	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
 	//test the perp method
 	n=12;
-	ThreeVector v12(4,3,980708);
+	ThreeVector<int> v12(4,3,980708);
 	if( v12.perp()==5){cout << "Test "<<n<<" passed!"<< endl;}
 	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
 
 	//test the output operator
 	n=13;
 	stringstream testStringStream, referenceStringStream;
+
 	testStringStream 		<< v12;
 	referenceStringStream	<< v12.x() <<"\t"<< v12.y() <<"\t"<< v12.z();
+
 	if(testStringStream.str() ==referenceStringStream.str() ){cout << "Test "<<n<<" passed!"<< endl;}
 	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
 
@@ -115,16 +126,16 @@ int main(int argc, char *argv[]) {
 //	cout << v6.x()<< " "<< v6.y()<< " "<< v6.z()<< " "<<endl;
 
 
-void allElementsEqual(const int &n,const ThreeVector &vA, const ThreeVector &vB){
+void allElementsEqual(const int &n,const ThreeVector<double> &vA, const ThreeVector<double> &vB){
 	if((vA.x()==vB.x())&&(vA.y()==vB.y())&&(vA.z()==vB.z())) {cout << "Test  "<<n<<" passed!"<< endl;}
 	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
 }
-void allElementsUnequal(const int &n,const ThreeVector &vA, const ThreeVector &vB){
+void allElementsUnequal(const int &n,const ThreeVector<double> &vA, const ThreeVector<double> &vB){
 	if((vA.x()!=vB.x())&&(vA.y()!=vB.y())&&(vA.z()!=vB.z())) {cout << "Test  "<<n<<" passed!"<< endl;}
 	else{cout << "Test  "<<n<<" FAILED!"<< endl;}
 }
 
-//ostream& operator<<(ostream& os, const ThreeVector& vOut){
+//ostream& operator<<(ostream& os, const ThreeVector<double>& vOut){
 //	os << vOut.x() <<"\t"<< vOut.y() <<"\t"<< vOut.z() <<"\t";
 //	return os;
 //}
